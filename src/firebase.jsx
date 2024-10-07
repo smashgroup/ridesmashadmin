c195b9f67043ad37b5bfa51019b64105d1f7b308
smashgroup <ridesmashnigeria@gmail.com>
2024-10-07 23:10:03 +0100

    new Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -6,13 +6,13 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
 
-  apiKey: "AIzaSyAAqFHkCyeC2sKMMMBLFQYvw2u0vBJIEZs",
-  authDomain: "https://ridesmash.com/",
-  projectId: "smashride-ee7a7",
-  storageBucket: "smashride-ee7a7.appspot.com",
-  messagingSenderId: "425620499927",
-  appId: "1:425620499927:web:3a5c333265b54b38f12f92",
-  measurementId: "G-4GL1H1TDQF"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 
 };
 
